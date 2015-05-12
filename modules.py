47812fcb92e689e614a275f0cb10028f510c2db5
Kunihiko Kido <kido@knowledge-works.co.jp>
2015-05-12 16:54:16 +0900

    Fiexed duplicate codes.

--- modules.py
+++ modules.py
@@ -1,4 +1,5 @@
 from .base import ElasticsearchCommand
+from .base import delete_ok_cancel_dialog
 
 
 class PutScriptCommand(ElasticsearchCommand):
@@ -15,28 +16,22 @@ class PutScriptCommand(ElasticsearchCommand):
         self.get_script_id(self.on_done)
 
     def on_done(self, id):
-        if not id:
-            return
-
-        body = self.selection()
-        self.request(self.esclient.put_script, self.lang, id, body=body)
+        self.request_api(
+            'put_script', lang=self.lang, id=id, body=self.selection())
 
 
 class GetScriptCommand(PutScriptCommand):
     result_window_title = "Get Script"
 
     def on_done(self, id):
-        if not id:
-            return
-
-        self.request(self.esclient.get_script, self.lang, id)
+        self.request_api('get_script', lang=self.lang, id=id)
 
 
 class DeleteScriptCommand(PutScriptCommand):
     result_window_title = "Delete Script"
 
     def on_done(self, id):
-        if not id:
+        if not delete_ok_cancel_dialog(id):
             return
 
-        self.request(self.esclient.delete_script, self.lang, id)
+        self.request_api('delete_script', lang=self.lang, id=id)
