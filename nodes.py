62ec26d9003d62e7e88839a7420ebfbadf4ab4e5
Kunihiko Kido <kido@knowledge-works.co.jp>
2015-05-12 16:50:50 +0900

    Fixed duplicate codes.

--- nodes.py
+++ nodes.py
@@ -1,41 +1,48 @@
 from .base import ElasticsearchCommand
 
 
-class NodesInfoCommand(ElasticsearchCommand):
+class NodesClientCommand(ElasticsearchCommand):
+    def request_nodes_api(self, method, *args, **kwargs):
+        method = getattr(self.esclient.nodes, method.lower())
+        kwargs = kwargs or {}
+        self.request(method, *args, **kwargs)
+
+
+class NodesInfoCommand(NodesClientCommand):
     result_window_title = "Nodes Info"
 
     def run(self):
         self.get_node_id(self.on_done)
 
     def on_done(self, node_id):
-        self.request(self.esclient.nodes.info, node_id=node_id)
+        self.request_nodes_api('info', node_id=node_id)
 
 
-class NodesShutdownCommand(ElasticsearchCommand):
+class NodesShutdownCommand(NodesClientCommand):
     result_window_title = "Nodes Shutdown"
 
     def run(self):
         self.get_node_id(self.on_done)
 
     def on_done(self, node_id):
-        self.request(self.esclient.nodes.shutdown, node_id=node_id)
+        self.request_nodes_api('shutdown', node_id=node_id)
 
 
-class NodesStatsCommand(ElasticsearchCommand):
+class NodesStatsCommand(NodesClientCommand):
     result_window_title = "Nodes Stats"
 
     def run(self):
         self.get_node_id(self.on_done)
 
     def on_done(self, node_id):
-        self.request(self.esclient.nodes.stats, node_id=node_id)
+        self.request_nodes_api('stats', node_id=node_id)
 
 
-class NodesHotThreadsCommand(ElasticsearchCommand):
+class NodesHotThreadsCommand(NodesClientCommand):
     result_window_title = "Nodes hot_threads"
 
     def run(self):
         self.get_node_id(self.on_done)
 
     def on_done(self, node_id):
-        self.request(self.esclient.nodes.hot_threads, node_id=node_id)
+        self.request_nodes_api('hot_threads', node_id=node_id)
