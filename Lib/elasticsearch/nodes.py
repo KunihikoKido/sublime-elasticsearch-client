0d811552747e0cda3fc460eaf48dc6f2a7f8d995
Kunihiko Kido <kido@knowledge-works.co.jp>
2015-06-04 09:56:04 +0900

    fixed bug

--- Lib/elasticsearch/nodes.py
+++ Lib/elasticsearch/nodes.py
@@ -1,6 +1,7 @@
 from .utils import BaseClient
 from .utils import make_path
 from .utils import show_result_json
+from .utils import show_result
 
 
 class NodesClient(BaseClient):
@@ -25,4 +26,4 @@ class NodesClient(BaseClient):
     def hot_threads(self, node_id=None, params=None, command=None):
         result = self.client.request(
             'GET', make_path('_nodes', node_id, 'hot_threads'), params=params)
-        return show_result_json(result.json(), command=command)
+        return show_result(result.text, command=command)
